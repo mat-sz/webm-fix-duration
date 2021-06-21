5ae26e4a2c560cc9205bd0269b8d2390a516b07f
Mat Sz <contact@matsz.dev>
2021-06-21 19:10:05 +0200

    basic typescript rewrite

--- src/fix-webm-duration.ts
+++ src/fix-webm-duration.ts
@@ -2,7 +2,13 @@
  * This is the list of possible WEBM file sections by their IDs.
  * Possible types: Container, Binary, Uint, Int, String, Float, Date
  */
-var sections = {
+
+interface Section {
+  name: string;
+  type: string;
+}
+
+const sections: Record<number, Section> = {
   0xa45dfa3: { name: 'EBML', type: 'Container' },
   0x286: { name: 'EBMLVersion', type: 'Uint' },
   0x2f7: { name: 'EBMLReadVersion', type: 'Uint' },
@@ -229,96 +235,115 @@ var sections = {
   0x485: { name: 'TagBinary', type: 'Binary' },
 };
 
-class WebmBase {
-  constructor(name, type) {
-    this.name = name || 'Unknown';
-    this.type = type || 'Unknown';
-  }
+class WebmBase<T> {
+  source?: Uint8Array;
+  data?: T;
+
+  constructor(private name = 'Unknown', private type = 'Unknown') {}
+
   updateBySource() {}
-  setSource(source) {
+
+  setSource(source: Uint8Array) {
     this.source = source;
     this.updateBySource();
   }
+
   updateByData() {}
-  setData(data) {
+
+  setData(data: T) {
     this.data = data;
     this.updateByData();
   }
 }
 
-class WebmUint extends WebmBase {
-  constructor(name, type) {
-    WebmBase.call(this, name, type || 'Uint');
+class WebmUint extends WebmBase<string> {
+  constructor(name: string, type: string) {
+    super(name, type || 'Uint');
   }
+
   updateBySource() {
     // use hex representation of a number instead of number value
     this.data = '';
-    for (var i = 0; i < this.source.length; i++) {
-      var hex = this.source[i].toString(16);
+    for (let i = 0; i < this.source!.length; i++) {
+      const hex = this.source![i].toString(16);
       this.data += padHex(hex);
     }
   }
+
   updateByData() {
-    var length = this.data.length / 2;
+    const length = this.data!.length / 2;
     this.source = new Uint8Array(length);
-    for (var i = 0; i < length; i++) {
-      var hex = this.data.substr(i * 2, 2);
+    for (let i = 0; i < length; i++) {
+      const hex = this.data!.substr(i * 2, 2);
       this.source[i] = parseInt(hex, 16);
     }
   }
+
   getValue() {
-    return parseInt(this.data, 16);
+    return parseInt(this.data!, 16);
   }
-  setValue(value) {
+
+  setValue(value: number) {
     this.setData(padHex(value.toString(16)));
   }
 }
 
-function padHex(hex) {
+function padHex(hex: string) {
   return hex.length % 2 === 1 ? '0' + hex : hex;
 }
 
-class WebmFloat extends WebmBase {
-  constructor(name, type) {
-    WebmBase.call(this, name, type || 'Float');
+class WebmFloat extends WebmBase<number> {
+  constructor(name: string, type: string) {
+    super(name, type || 'Float');
   }
+
   getFloatArrayType() {
     return this.source && this.source.length === 4
       ? Float32Array
       : Float64Array;
   }
   updateBySource() {
-    var byteArray = this.source.reverse();
-    var floatArrayType = this.getFloatArrayType();
-    var floatArray = new floatArrayType(byteArray.buffer);
-    this.data = floatArray[0];
+    const byteArray = this.source!.reverse();
+    const floatArrayType = this.getFloatArrayType();
+    const floatArray = new floatArrayType(byteArray.buffer);
+    this.data! = floatArray[0];
   }
   updateByData() {
-    var floatArrayType = this.getFloatArrayType();
-    var floatArray = new floatArrayType([this.data]);
-    var byteArray = new Uint8Array(floatArray.buffer);
+    const floatArrayType = this.getFloatArrayType();
+    const floatArray = new floatArrayType([this.data!]);
+    const byteArray = new Uint8Array(floatArray.buffer);
     this.source = byteArray.reverse();
   }
   getValue() {
     return this.data;
   }
-  setValue(value) {
+  setValue(value: number) {
     this.setData(value);
   }
 }
 
-class WebmContainer extends WebmBase {
-  constructor(name, type) {
-    WebmBase.call(this, name, type || 'Container');
+interface ContainerData {
+  id: number;
+  idHex?: string;
+  data: WebmBase<any>;
+}
+
+class WebmContainer extends WebmBase<ContainerData[]> {
+  offset: number = 0;
+  data: ContainerData[] = [];
+
+  constructor(name: string, type: string) {
+    super(name, type || 'Container');
   }
+
   readByte() {
-    return this.source[this.offset++];
+    return this.source![this.offset++];
   }
   readUint() {
-    var firstByte = this.readByte();
-    var bytes = 8 - firstByte.toString(2).length;
-    var value = firstByte - (1 << (7 - bytes));
-    for (var i = 0; i < bytes; i++) {
+    const firstByte = this.readByte();
+    const bytes = 8 - firstByte.toString(2).length;
+    let value = firstByte - (1 << (7 - bytes));
+    for (let i = 0; i < bytes; i++) {
       // don't use bit operators to support x86
       value *= 256;
       value += this.readByte();
@@ -326,15 +351,20 @@ class WebmContainer extends WebmBase {
     return value;
   }
   updateBySource() {
+    let end: number | undefined = undefined;
     this.data = [];
-    for (this.offset = 0; this.offset < this.source.length; this.offset = end) {
-      var id = this.readUint();
-      var len = this.readUint();
-      var end = Math.min(this.offset + len, this.source.length);
-      var data = this.source.slice(this.offset, end);
+    for (
+      this.offset = 0;
+      this.offset < this.source!.length;
+      this.offset = end
+    ) {
+      const id = this.readUint();
+      const len = this.readUint();
+      end = Math.min(this.offset + len, this.source!.length);
+      const data = this.source!.slice(this.offset, end);
 
-      var info = sections[id] || { name: 'Unknown', type: 'Unknown' };
-      var ctr = WebmBase;
+      const info = sections[id] || { name: 'Unknown', type: 'Unknown' };
+      let ctr: any = WebmBase;
       switch (info.type) {
         case 'Container':
           ctr = WebmContainer;
@@ -346,7 +376,7 @@ class WebmContainer extends WebmBase {
           ctr = WebmFloat;
           break;
       }
-      var section = new ctr(info.name, info.type);
+      const section = new ctr(info.name, info.type);
       section.setSource(data);
       this.data.push({
         id: id,
@@ -355,7 +385,7 @@ class WebmContainer extends WebmBase {
       });
     }
   }
-  writeUint(x, draft) {
+  writeUint(x: number, draft = false) {
     for (
       var bytes = 1, flag = 0x80;
       x >= flag && bytes < 8;
@@ -363,74 +393,81 @@ class WebmContainer extends WebmBase {
     ) {}
 
     if (!draft) {
-      var value = flag + x;
-      for (var i = bytes - 1; i >= 0; i--) {
+      let value = flag + x;
+      for (let i = bytes - 1; i >= 0; i--) {
         // don't use bit operators to support x86
-        var c = value % 256;
-        this.source[this.offset + i] = c;
+        const c = value % 256;
+        this.source![this.offset! + i] = c;
         value = (value - c) / 256;
       }
     }
 
     this.offset += bytes;
   }
-  writeSections(draft) {
+
+  writeSections(draft = false) {
     this.offset = 0;
-    for (var i = 0; i < this.data.length; i++) {
-      var section = this.data[i],
+    for (let i = 0; i < this.data.length; i++) {
+      const section = this.data[i],
         content = section.data.source,
-        contentLength = content.length;
+        contentLength = content!.length;
       this.writeUint(section.id, draft);
       this.writeUint(contentLength, draft);
       if (!draft) {
-        this.source.set(content, this.offset);
+        this.source!.set(content!, this.offset);
       }
       this.offset += contentLength;
     }
     return this.offset;
   }
+
   updateByData() {
     // run without accessing this.source to determine total length - need to know it to create Uint8Array
-    var length = this.writeSections('draft');
+    const length = this.writeSections(true);
     this.source = new Uint8Array(length);
     // now really write data
     this.writeSections();
   }
-  getSectionById(id) {
-    for (var i = 0; i < this.data.length; i++) {
-      var section = this.data[i];
+
+  getSectionById(id: number) {
+    for (let i = 0; i < this.data.length; i++) {
+      const section = this.data[i];
       if (section.id === id) {
         return section.data;
       }
     }
-    return null;
+
+    return undefined;
   }
 }
 
 class WebmFile extends WebmContainer {
-  constructor(source) {
-    WebmContainer.call(this, 'File', 'File');
+  constructor(source: Uint8Array) {
+    super('File', 'File');
     this.setSource(source);
   }
-  fixDuration(duration) {
-    var segmentSection = this.getSectionById(0x8538067);
+
+  fixDuration(duration: number) {
+    const segmentSection = this.getSectionById(0x8538067) as WebmContainer;
     if (!segmentSection) {
       return false;
     }
 
-    var infoSection = segmentSection.getSectionById(0x549a966);
+    const infoSection = segmentSection.getSectionById(
+      0x549a966
+    ) as WebmContainer;
     if (!infoSection) {
       return false;
     }
 
-    var timeScaleSection = infoSection.getSectionById(0xad7b1);
+    const timeScaleSection = infoSection.getSectionById(0xad7b1) as WebmFloat;
     if (!timeScaleSection) {
       return false;
     }
 
-    var durationSection = infoSection.getSectionById(0x489);
+    let durationSection = infoSection.getSectionById(0x489) as WebmFloat;
     if (durationSection) {
-      if (durationSection.getValue() <= 0) {
+      if (durationSection.getValue()! <= 0) {
         durationSection.setValue(duration);
       } else {
         return false;
@@ -453,18 +490,21 @@ class WebmFile extends WebmContainer {
 
     return true;
   }
+
   toBlob() {
-    return new Blob([this.source.buffer], { type: 'video/webm' });
+    return new Blob([this.source!.buffer], { type: 'video/webm' });
   }
 }
 
-export default function (blob, duration) {
+export default function (blob: Blob, duration: number) {
   return new Promise((resolve, reject) => {
     try {
-      var reader = new FileReader();
-      reader.onloadend = function () {
+      const reader = new FileReader();
+
+      reader.addEventListener('loadend', () => {
         try {
-          var file = new WebmFile(new Uint8Array(reader.result));
+          const result = reader.result as ArrayBuffer;
+          const file = new WebmFile(new Uint8Array(result));
           if (file.fixDuration(duration)) {
             blob = file.toBlob();
           }
@@ -472,7 +512,9 @@ export default function (blob, duration) {
           reject(ex);
         }
         resolve(blob);
-      };
+      });
+
+      reader.addEventListener('error', () => reject());
 
       reader.readAsArrayBuffer(blob);
     } catch (ex) {
